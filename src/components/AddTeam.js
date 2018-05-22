1fe9a24b63110b7a9c116695e24580be3f32f38f
Hervis Pichardo <hervis15@gmail.com>
2018-05-22 08:11:22 -0400

    add more inputs

--- src/components/AddTeam.js
+++ src/components/AddTeam.js
@@ -1,37 +1,51 @@
 import React from 'react'
 import {connect} from 'dva'
 import styles from './AddTeam.css'
-import { Card, Input, Button } from 'antd'
+import { Card, Input, Button, Col } from 'antd'
 
 function AddTeam({teams, dispatch}) {
 
-  let inputTeam;
+  let teamName, teamCode, teamKms;
 
-  const createTeam = (name, goals, rating) => {
+  const createTeam = (name, goals, rating, kms, code) => {
     return {
       name: name,
       goals: 5,
       ranking: 4,
-      kms: 3256,
+      kms: kms,
       years: ["1964", "1994", "2002"],
       players: 5,
-      position: 'Campeon'
+      position: 'Campeon',
+      code: code
     }
   }
 
   const add = () => {
-    const value = inputTeam.input.value;
-    const team = createTeam(value, 5,4);
+    let name = teamName.input.value;
+    let code = teamCode.input.value;
+    let kms = teamKms.input.value;
+    const team = createTeam(name, 5,4, kms, code);
     console.log("add: ", team);
     dispatch({type: 'teams/addTeam', payload: team})
+
   }
 
   return (
     <div className={styles.normal}>
       <div style={{ background: '#ECECEC', padding: '30px' }}>
         <Card title="Add Team">
-          <Input placeholder="Team" ref={c => (inputTeam = c)}/>
-          <Button onClick={add} type="primary">Add</Button>
+          <Col span={6}>
+            <Input placeholder="Team" ref={c => (teamName = c)}/>
+          </Col>
+          <Col span={6}>
+            <Input placeholder="Code" ref={c => (teamCode = c)}/>
+          </Col>
+          <Col span={6}>
+            <Input placeholder="Kms" ref={c => (teamKms = c)}/>
+          </Col>
+          <Col span={6}>
+            <Button onClick={add} type="primary">Add</Button>
+          </Col>
         </Card>
       </div>
     </div>
